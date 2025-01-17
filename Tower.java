f724878fd048f5887f8c5cc2fc66bb66fb5b227c
chanyue <73543114+linglingling2002@users.noreply.github.com>
2025-01-17 13:44:25 +0800

    Create Tower.java
    Tower class

---
+++ Tower.java
@@ -0,0 +1,107 @@
+package gameClass;
+
+public class Tower {
+
+	/** 攻击力 */
+	private int attackValue;
+
+	/** 攻击速度 */
+	private int attackSpeed;
+
+	/** 射程 */
+	private int range;
+
+	/** 价格 */
+	private int price;
+
+	/** 横坐标 */
+	private int x;
+
+	/** 纵坐标 */
+	private int y;
+
+	/**
+	 * @return the attackValue
+	 */
+	public int getAttackValue() {
+		return attackValue;
+	}
+
+	/**
+	 * @param attackValue the attackValue to set
+	 */
+	public void setAttackValue(int attackValue) {
+		this.attackValue = attackValue;
+	}
+
+	/**
+	 * @return the attackSpeed
+	 */
+	public int getAttackSpeed() {
+		return attackSpeed;
+	}
+
+	/**
+	 * @param attackSpeed the attackSpeed to set
+	 */
+	public void setAttackSpeed(int attackSpeed) {
+		this.attackSpeed = attackSpeed;
+	}
+
+	/**
+	 * @return the range
+	 */
+	public int getRange() {
+		return range;
+	}
+
+	/**
+	 * @param range the range to set
+	 */
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
